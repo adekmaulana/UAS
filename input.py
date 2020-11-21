9e777b58623f8b7851fd8f91be6e166026fb2bc7
Adek Maulana <adekzmaulana@gmail.com>
2020-11-22 05:26:54 +0700

    receipt: tambah fitur untuk cetak struk belanja
    Change-Id: If7bf8af6ec36b42a9fd45858f5cf634cf10ae0fc

--- input.py
+++ input.py
@@ -1,19 +1,11 @@
 import sys
 import datetime
-import locale
 
 from random import sample
+from receipt import parse_data, parse_order, convert, rupiah_format
 from time import sleep
 from shutil import get_terminal_size
 from sql_helper.manager import get_item, update_item
-
-
-def rupiah_format(angka, with_prefix=True, desimal=0):
-    locale.setlocale(locale.LC_NUMERIC, 'id_ID.UTF-8')
-    rupiah = locale.format_string("%.*f", (desimal, angka), True)
-    if with_prefix:
-        return "Rp. {}".format(rupiah)
-    return rupiah
 
 
 def clear():
@@ -71,14 +63,12 @@ if __name__ == "__main__":
             print("Kode boxer tidak terdaftar!")
             sleep(2.5)
             clear()
-            cetak_katalog()
             continue
         # Jika ukuran tidak terdaftar, ulang looping
         if harga is None:
             print("Maaf ukuran belum ada...")
             sleep(2.5)
             clear()
-            cetak_katalog()
             continue
         banyak_beli = int(input("Masukkan banyak beli: "))
         # Check stock boxer pada database
@@ -88,13 +78,11 @@ if __name__ == "__main__":
             print(f"Maaf, {boxer.warna.upper()} ({ukuran.upper()}) belum tersedia")
             sleep(2.5)
             clear()
-            cetak_katalog()
             continue
         elif banyak_beli > quantity:
             print(f"Maaf, {boxer.warna.upper()} ({ukuran.upper()}) hanya tersedia {quantity}")
             sleep(2.5)
             clear()
-            cetak_katalog()
             continue
         list_boxer.append(boxer.warna)
         list_harga.append(harga)
@@ -108,8 +96,10 @@ if __name__ == "__main__":
         if lagi.upper() == "Y":
             clear()
             continue
-        else:
+        elif lagi.upper() == "N":
             break
+        else:
+            print('Jawaban anda diluar pertanyaan')
     clear()
     d = datetime.datetime.now()
     no = sample(range(1, 100), 3)
@@ -118,18 +108,27 @@ if __name__ == "__main__":
     print()
     print(f"{d:%d}/{d:%m}/{d:%y} {d:%-I}:{d:%M}:{d:%S} {d:%p}\t  ORDER: {order}")
     print()
+
     total_bayar = []
+    list_belanja = []
+
+    for i in range(jumlah_beli):
+        list_belanja.append(
+            f"{list_banyak[i]} {list_boxer[i].upper()} ({list_ukuran[i]})")
+
     for i in range(jumlah_beli):
         jumlah = list_harga[i] * list_banyak[i]
         total_bayar.append(jumlah)
-        print(f"\t{list_banyak[i]} {list_boxer[i].upper()} ({list_ukuran[i]})\t| "
-              f"{rupiah_format(jumlah)}")
+        print(f"\t{list_belanja[i]}\t| {rupiah_format(jumlah)}")
+
     raw_total = sum(total_bayar)
     tax = round(raw_total * 0.1)
     total = raw_total + tax
+
     print(f"\tSUBTOTAL\t| {rupiah_format(raw_total)}")
     print(f"\tPPN 10%\t\t| {rupiah_format(tax)}")
     print(f"\tTOTAL\t\t| {rupiah_format(total)}")
+
     uang_bayar = int(input("\tUANG\t\t| Rp. "))
     # Loop jika uang bayar kurang dari total bayar
     while uang_bayar < total:
@@ -141,3 +140,13 @@ if __name__ == "__main__":
     sys.stdout.write("\033[1A\033[2K")
     print(f"\tUANG\t\t| {rupiah_format(uang_bayar)}")
     print(f"\tCHANGE\t\t| {kembali}")
+    print()
+
+    data = [raw_total, tax, total, uang_bayar, uang_bayar - total]
+
+    ans = input("Cetak struk belanja? '[Y/N]': ")
+    if ans.upper() == "Y":
+        parse_data(d, no)
+        parse_order(list_belanja, total_bayar, data)
+        convert()
+        print("Struk belanja berhasil dicetak dengan nama 'receipt.png'")
--- receipt.py
+++ receipt.py
@@ -1,1 +1,164 @@
+import os
+import sys
+import datetime
+import cloudconvert
+import locale
+
+from contextlib import contextmanager
 from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.enum.style import WD_STYLE_TYPE
+
+
+def rupiah_format(angka, with_prefix=True, desimal=0):
+    locale.setlocale(locale.LC_NUMERIC, 'id_ID.UTF-8')
+    rupiah = locale.format_string("%.*f", (desimal, angka), True)
+    if with_prefix:
+        return "Rp. {}".format(rupiah)
+    return rupiah
+
+
+@contextmanager
+def suppress_stdout():
+    """ Didapat dari https://stackoverflow.com/a/60327812 """
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
+def convert() -> None:
+    tasks = {
+        "tasks": {
+            "upload-file": {
+                "operation": "import/upload",
+                "input_format": "docx",
+                "output_format": "pdf"
+            },
+            "convert-file-pdf": {
+                "operation": "convert",
+                "input": "upload-file",
+                "input_format": "docx",
+                "output_format": "pdf",
+                "engine": "office",
+                "engine_version": "2019",
+                "filename": "temp.pdf",
+                "optimize_print": True
+            },
+            "convert-file-png": {
+                "operation": "convert",
+                "input": "convert-file-pdf",
+                "input_format": "pdf",
+                "output_format": "png",
+                "engine": "poppler",
+                "engine_version": "0.71.0",
+                "filename": "receipt.png"
+            },
+            "download-converted-file": {
+                "operation": "export/url",
+                "input": "convert-file-png"
+            }
+        }
+    }
+
+    # Initialized API_KEY
+    cloudconvert.configure(api_key=API_KEY)
+
+    # Buat job pada server
+    job = cloudconvert.Job.create(payload=tasks)
+
+    # Upload file ke server
+    upload_task_id = job['tasks'][0]['id']
+    upload_task = cloudconvert.Task.find(id=upload_task_id)
+    res = cloudconvert.Task.upload(file_name='temp.docx', task=upload_task)
+
+    # mulai convert pada server
+    convert_task_id = [job['tasks'][1]['id'], job['tasks'][2]['id']]
+    res = cloudconvert.Task.wait(id=convert_task_id[0])  # docx to pdf
+    res = cloudconvert.Task.wait(id=convert_task_id[1])  # pdf to png
+
+    # download converted file di server ke local
+    download_task_id = job['tasks'][3]['id']
+    res = cloudconvert.Task.wait(id=download_task_id)
+    files = res.get("result").get("files")[0]
+    with suppress_stdout():
+        res = cloudconvert.download(
+            filename=files['filename'], url=files['url'])
+
+    os.remove('temp.docx')
+
+
+def parse_data(d: datetime.datetime, no: list) -> None:
+    order = f"#AV{no[0]}{no[1]}{no[2]}"
+    cells_text = [
+        f"{d:%d}/{d:%m}/{d:%y} {d:%-I}:{d:%M}:{d:%S} {d:%p}",
+        f"{order}"
+    ]
+    document = Document('receipt.docx')
+    table = document.tables[1].rows[0]
+
+    # styles = document.styles
+    # style = styles.add_style('Abadi MT Std', WD_STYLE_TYPE.PARAGRAPH)
+    # style.font.name = 'Abadi MT Std'
+    for index, cell in enumerate(table.cells):
+        paragraphs = cell.paragraphs
+        for paragraph in paragraphs:
+            run = paragraph.add_run()
+            font = run.font
+            font.name = 'Abadi MT Std'
+            font.size = Pt(12)
+            font.bold = True
+            run.text = cells_text[index]
+    return document.save('temp.docx')
+
+
+def parse_order(list_belanja: list, total_bayar: list, data: list) -> None:
+    TEXT = ["SUBTOTAL", "PPN 10%", "TOTAL", "UANG", "CHANGE"]
+
+    document = Document('temp.docx')
+    table = document.tables[2]
+
+    total = len(list_belanja) + 5
+    for row in range(total):
+        table.add_row()
+    rows = table.rows
+
+    # Tulis belanjaan pada struk belanja
+    for index, belanja in enumerate(list_belanja):
+        for i, cell in enumerate(rows[index].cells):
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                run = paragraph.add_run()
+                font = run.font
+                font.name = 'Abadi MT Std'
+                font.size = Pt(12)
+                font.bold = True
+                if i == 0:
+                    paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+                    run.text = list_belanja[index]
+                elif i == 1:
+                    run.text = rupiah_format(total_bayar[index])
+
+    # Tulis SUBTOTAL, PPN, TOTAL, UANG, CHANGE/KEMBALIAN
+    i = 4
+    while i <= total:
+        for index, cell in enumerate(rows[i].cells):
+            paragraphs = cell.paragraphs
+            for paragraph in paragraphs:
+                run = paragraph.add_run()
+                font = run.font
+                font.name = 'Abadi MT Std'
+                font.size = Pt(12)
+                font.bold = True
+                if index == 0:
+                    paragraph.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.RIGHT
+                    run.text = TEXT[i - 4]
+                elif index == 1:
+                    run.text = rupiah_format(data[i - 4])
+        i += 1
+
+    document.save('temp.docx')
